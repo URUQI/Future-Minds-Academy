0f72c4989e7536e619d73255b0d6263775813cf9
URUQI <149891790+URUQI@users.noreply.github.com>
2024-06-11 21:01:56 +0200

    kode te shkurtra

---
+++ Sessions/artons-class/snipped-code/questions.js
@@ -0,0 +1,22 @@
+let allQuestions = [
+        {
+                question: 'what does HTML stand for?',
+                answer: 'Hyper Text Markup Language',
+                options: [
+                        'Hyper Text Making Language',
+                        'Hyper Text Markup Language',
+                        'HipHop Making Language',
+                        'Hinting Markup Language'
+                ]
+        },
+        {
+                question: 'Wich protocol we use to secure our webpage?',
+                answer: 'HTTPS',
+                options: [
+                        'HTTP',
+                        'TCPIP',
+                        'SSH',
+                        'HTTPS'
+                ]
+        }
+];
